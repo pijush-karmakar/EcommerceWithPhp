3e7e94877daccd149c2380e619971d9320bfb488
pijush karmakar <pijushkarmakar45@gmail.com>
2018-02-28 12:14:25 +0600

    Update index.php

--- EcommerceWithPhp/index.php
+++ EcommerceWithPhp/index.php
@@ -21,11 +21,11 @@
 
 	      	 ?>
 				<div class="grid_1_of_4 images_1_of_4">
-					 <a href="details.php?proid=<?php echo $result['productId']; ?>"><img src="admin/<?php echo $result['image']; ?>" alt="" /></a>
+					 <a href="details.php?productid=<?php echo $result['productId']; ?>"><img src="admin/<?php echo $result['image']; ?>" alt="" /></a>
 					 <h2> <?php echo $result['productName']; ?></h2>
 					 <p><?php echo $fm->textShorten($result['description'],100 ) ; ?></p>
 					 <p><span class="price">$<?php echo $result['price']; ?></span></p>
-				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+				     <div class="button"><span><a href="details.php?productid=<?php echo $result['productId']; ?>" class="details">Details</a></span></div>
 				</div>
 
 				<?php } } ?>
@@ -38,31 +38,22 @@
     		<div class="clear"></div>
     	</div>
 			<div class="section group">
-				<div class="grid_1_of_4 images_1_of_4">
-					 <a href="details.php"><img src="images/new-pic1.jpg" alt="" /></a>
-					 <h2>Lorem Ipsum is simply </h2>
-					 <p><span class="price">$403.66</span></p>
-				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
-				</div>
-				<div class="grid_1_of_4 images_1_of_4">
-					<a href="details.php"><img src="images/new-pic2.jpg" alt="" /></a>
-					 <h2>Lorem Ipsum is simply </h2>
-					 <p><span class="price">$621.75</span></p> 
-				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
-				</div>
-				<div class="grid_1_of_4 images_1_of_4">
-					<a href="details.php"><img src="images/feature-pic2.jpg" alt="" /></a>
-					 <h2>Lorem Ipsum is simply </h2>
-					 <p><span class="price">$428.02</span></p>
-				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
-				</div>
-				<div class="grid_1_of_4 images_1_of_4">
-				 <img src="images/new-pic3.jpg" alt="" />
-					 <h2>Lorem Ipsum is simply </h2>					 
-					 <p><span class="price">$457.88</span></p>
+			<?php 
 
-				     <div class="button"><span><a href="details.php" class="details">Details</a></span></div>
+	             $getNpd = $pd->getNewProduct();
+	             if($getNpd){
+	             	while($result = $getNpd->fetch_assoc() ){
+
+	      	 ?>
+				<div class="grid_1_of_4 images_1_of_4">
+					<a href="details.php?productid=<?php echo $result['productId']; ?>"><img src="admin/<?php echo $result['image']; ?>" alt="" /></a>
+					  <h2> <?php echo $result['productName']; ?></h2>
+					 <p><span class="price">$<?php echo $result['price']; ?></span></p>
+				     <div class="button"><span><a href="details.php?productid=<?php echo $result['productId']; ?>" class="details">Details</a></span></div>
 				</div>
+
+				<?php } } ?>
+				
 			</div>
     </div>
  </div>
